053cd01c28d41cf408e504b7ae0e52f51c9dae35
Shreyash Khaladkar <shreyashkhaladkar2@gmail.com>
2025-09-15 02:25:17 +0530

    Initial commit - Tic Tac Toe game

---
+++ scripts/config.js
@@ -0,0 +1,38 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
+
+  const input = document.getElementById("playername");
+  input.value = "";                // clear text
+  input.blur();                    // remove focus (prevents browser suggestion)
+  setTimeout(() => input.focus(), 10); // re-focus cleanly
+
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+
+function closePlayerConfig(){
+    playerConfigOverlayElement.style.display= 'none';
+     backdropElement.style.display= 'none';
+     formElement.firstElementChild.classList.remove('error')
+     formElement.firstElementChild.lastElementChild.value = '';
+}
+
+function savePlayerConfig(event){
+    event.preventDefault();
+    const formData= new FormData(event.target);
+    const enteredPlayername= formData.get('playername').trim()
+
+    if(!enteredPlayername){
+        event.target.firstElementChild.classList.add('error')
+        errorsOutputElement.textContent='Please enter a valid name! '
+        return;
+    }
+    const updatedPlayerDataElement= document.getElementById('player-'+ editedPlayer+ '-data')
+    updatedPlayerDataElement.children[1].textContent= enteredPlayername;
+
+        players[editedPlayer - 1].name=enteredPlayername;
+        closePlayerConfig();
+    
+
+}   
